42be9c8f674aed8c525471bb41bd61e2b9d9cec6
murtll <jkoz@inbox.ru>
2020-07-28 19:13:54 +0300

    Cleanup

--- src/test/java/kz/datcom/test/TestApplicationTests.java
+++ src/test/java/kz/datcom/test/TestApplicationTests.java
@@ -27,80 +27,25 @@ import java.util.Scanner;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureMockMvc
 class TestApplicationTests {
-
-    @Test
-    void testQRGen() {
-        File file = QRCode.from((new Date()).toString()).file();
-        System.out.println(file.getAbsolutePath());
-    }
-
-    @Test
-    void testFileCopy() {
-
-        OutputStream os = null;
-        try {
-            os = new FileOutputStream(new File("files\\output\\file.jpg"));
-            QRCode.from(new Date().toString()).to(ImageType.JPG).withSize(50, 50).writeTo(os);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }finally{
-            try {
-                os.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Test
-    void testInserting() throws IOException {
-        File file = new File("D:\\murtll\\work\\datcom\\projects\\test\\files\\output\\output.pdf");
-        PDDocument doc = PDDocument.load(file);
-
-        //Retrieving the page
-
-        PDPage page = doc.getPage(0);
-
-        //Creating PDImageXObject object
-        PDImageXObject pdImage = PDImageXObject.createFromFile("D:\\murtll\\work\\datcom\\projects\\test\\files\\output\\file.jpg",doc);
-
-        //creating the PDPageContentStream object
-        PDPageContentStream contents = new PDPageContentStream(doc, page);
-
-        //Drawing the image in the PDF document
-        contents.drawImage(pdImage, 70, 250);
-
-        System.out.println("Image inserted");
-
-        //Closing the PDPageContentStream object
-        contents.close();
-
-        //Saving the document
-        doc.save(file.getAbsolutePath());
-
-        //Closing the document
-        doc.close();
-    }
-
     @Autowired
     PDFService service;
-
-    @Test
-    void testMyInserting() {
-
-        service.addQRsToPDF(new File("D:\\murtll\\work\\datcom\\projects\\test\\files\\output\\output.pdf"), new Date().toString());
-
-    }
 
     @Test
     void testMergePdfs() {
 
         List<File> list = new ArrayList<>();
 
-        list.add(new File("D:\\murtll\\work\\datcom\\projects\\test\\files\\input\\inf.pdf"));
-        list.add(new File("D:\\murtll\\work\\datcom\\projects\\test\\files\\input\\mat.pdf"));
+        list.add(new File("D:\\murtll\\work\\datcom\\projects\\test\\files\\input\\description.pdf"));
+        list.add(new File("D:\\murtll\\work\\datcom\\projects\\test\\files\\input\\agreement.pdf"));
 
         service.mergePDFs(list);
+
+    }
+
+    @Test
+    void testMyInserting() {
+
+        service.addQRsToPDF(new File("D:\\murtll\\work\\datcom\\projects\\test\\files\\output\\output.pdf"), new Date().toString());
 
     }
 
